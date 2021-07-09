090f485e28c9da52b0756f14d8985ec75b2d8700
jieyr3Rui <2930681195@qq.com>
2021-07-09 11:32:30 +0800

    debug

--- server.py
+++ server.py
@@ -1,30 +1,34 @@
 import socket
 import threading
-address='127.0.0.1' 
-port=12344 
-buffsize=1024     
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((address,port))
-s.listen(3)   
+
 
 def tcplink(sock,addr):
     while True:  
-        recv_data=clientsock.recv(buffsize).decode('utf-8')
+        recv_data=sock.recv(buffsize).decode('utf-8')
         if recv_data=='exit' or not recv_data:
             break
         print("form client: " + str(addr) +", data:  " + recv_data)
         send_data= "from sever: [" + recv_data + "]"
-        clientsock.send(send_data.encode())
+        sock.send(send_data.encode())
 
-    clientsock.close()
+    sock.close()
 
-print("connecting to clients...")
-ii = 0
-while True:
-    ii = ii + 1
-    # create a new sock for the client
-    clientsock,clientaddress=s.accept()
-    print("connect to the " + str(ii) + "th client: ",clientaddress)
-    t=threading.Thread(target=tcplink,args=(clientsock,clientaddress)) 
-    t.start()
-s.close()
+if __name__ == "__main__":
+    address='127.0.0.1' 
+    port=12344 
+    buffsize=1024     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((address,port))
+    s.listen(3)   
+
+
+    print("connecting to clients...")
+    ii = 0
+    while True:
+        ii = ii + 1
+        # create a new sock for the client
+        clientsock,clientaddress=s.accept()
+        print("connect to the " + str(ii) + "th client: ",clientaddress)
+        t=threading.Thread(target=tcplink,args=(clientsock,clientaddress)) 
+        t.start()
+    s.close()
