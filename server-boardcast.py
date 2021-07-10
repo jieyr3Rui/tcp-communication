ef4f8deeb23b6c4e968d9611742c849d0237c014
jieyr3Rui <38846961+jieyr3Rui@users.noreply.github.com>
2021-07-10 11:18:55 +0800

    Update server-boardcast.py

--- server-boardcast.py
+++ server-boardcast.py
@@ -6,22 +6,18 @@ def keyboard():
     global sock_l
     msg = ""
     while True:
-        msg=input('Please input: ')
-        if not msg:
-            continue
-        else:
-            for sock in sock_l:
-                print("send")
-                sock.send(msg.encode())
+        if len(sock_l) > 0:
+            msg=raw_input('Please input: ')
+            if not msg:
+                continue
+            else:
+                for sock in sock_l:
+                    sock.send(msg.encode())
     return
 
 if __name__ == "__main__":
     global sock_l
     sock_l = []
-
-    tk=threading.Thread(target=keyboard,args=()) 
-    tk.start()
-
     address='0.0.0.0' 
     port=12344 
     buffsize=1024     
@@ -32,12 +28,17 @@ if __name__ == "__main__":
 
     print("connecting to clients...")
     ii = 0
+
+    tk=threading.Thread(target=keyboard,args=()) 
+    tk.start()
+
     while True:
         ii = ii + 1
         # create a new sock for the client
         clientsock,clientaddress=s.accept()
-        print("connect to the " + str(ii) + "th client: ",clientaddress)
+        print("connect to the " , str(ii) , "th client: ", clientaddress)
         sock_l.append(clientsock)
+    
 
     s.close()
     
